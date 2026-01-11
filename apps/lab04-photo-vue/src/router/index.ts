3d7e50cd50a38b0b706127216b0350e5d487b292
arthitayasararat <arthitaya.sa@kkumail.com>
2026-01-11 21:28:14 +0700

    lab04 photo gallery complete

---
+++ apps/lab04-photo-vue/src/router/index.ts
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import TabsPage from '@/views/TabsPage.vue';
+import Tab1Page from '@/views/Tab1Page.vue';
+import Tab2Page from '@/views/Tab2Page.vue';
+import Tab3Page from '@/views/Tab3Page.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/tabs/tab2'
+  },
+  {
+    path: '/tabs/',
+    component: TabsPage,
+    children: [
+      {
+        path: '',
+        redirect: '/tabs/tab1'
+      },
+      {
+        path: 'tab1',
+        component: Tab1Page
+      },
+      {
+        path: 'tab2',
+        component: Tab2Page
+      },
+      {
+        path: 'tab3',
+        component: Tab3Page
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ apps/lab04-photo-vue/src/views/Tab2Page.vue
@@ -0,0 +1,59 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery <br>โดย อาทิตยา สารราษฎร์ 673380256-9</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Photo Gallery</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-grid>
+    <ion-row>
+      <ion-col
+        size="6"
+        v-for="photo in photos"
+        :key="photo.filepath"
+      >
+        <ion-img :src="photo.webviewPath" />
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+
+      
+
+      <!-- CHANGE: Remove or comment out <ExploreContainer /> -->
+      <!-- <ExploreContainer name="Tab 2 page" /> -->
+    </ion-content>
+    <!-- CHANGE: Add the floating action button -->
+      <ion-fab vertical="bottom" horizontal="center" >
+        <ion-fab-button @click="addNewToGallery()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+// CHANGE: Add import from `ionicons/icons`
+import { camera } from 'ionicons/icons';
+// CHANGE: Update import from `@ionic/vue` to include necessary Ionic components
+import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+// CHANGE: Remove or comment out the ExploreContainer import
+// import ExploreContainer from '@/components/ExploreContainer.vue';
+// CHANGE: Add `usePhotoGallery` import
+import { usePhotoGallery } from "../composables/usePhotoGallery";
+
+// CHANGE: Destructure `addNewToGallery` from `usePhotoGallery()
+const { photos,addNewToGallery } = usePhotoGallery();
+import {
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg
+} from '@ionic/vue';
+</script>
+
